24b2b12a2a7f82b327ac30aca43a4fcab4f3773d
jianzhi-1 <jianzhi@berkeley.edu>
2025-02-15 20:23:49 -0800

    added analysis stage

--- app/push_to_talk_app.py
+++ app/push_to_talk_app.py
@@ -16,6 +16,7 @@
 #     "pydub",
 #     "sounddevice",
 #     "openai[realtime]",
+#     "pyautogui"
 # ]
 #
 # [tool.uv.sources]
@@ -25,11 +26,12 @@ from __future__ import annotations
 
 import base64
 import asyncio
-from typing import Any, cast
+from typing import Any, cast, Tuple, Union, List
 from typing_extensions import override
 import logging
 import time
 import pyautogui
+import subprocess
 
 from textual import events
 from audio_util import CHANNELS, SAMPLE_RATE, AudioPlayerAsync
@@ -41,6 +43,9 @@ from textual.containers import Container
 from openai import AsyncOpenAI
 from openai.types.beta.realtime.session import Session
 from openai.resources.beta.realtime.realtime import AsyncRealtimeConnection
+
+from random import choice
+from string import ascii_uppercase
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='berk.log', encoding='utf-8', level=logging.DEBUG)
@@ -143,6 +148,13 @@ class RealtimeApp(App[None]):
         self.last_audio_item_id = None
         self.should_send_audio = asyncio.Event()
         self.connected = asyncio.Event()
+        self.a0 = None # accumulator, stores return value
+        self.t0 = None # task instruction
+        self.GPT_MODEL = "gpt-4o-mini"
+        self.REALTIME_MODEL = "gpt-4o-realtime-preview"
+        self.screenshot_name_history = []
+        self.screenshot_name_length = 20
+        self.KEYWORDS = ["<enter>"]
 
     @override
     def compose(self) -> ComposeResult:
@@ -158,40 +170,66 @@ class RealtimeApp(App[None]):
 
     async def assemble_and_run(self, transcript):
         instructions = await self.codegen(transcript)
-        self.execute(instructions)
+        await self.execute(instructions, transcript)
 
     async def codegen(self, transcript) -> list[str]:
         """
         Acts as an intermediate language.
         """
         response = await self.client.chat.completions.create(
-            model="gpt-4o-mini",
+            model=self.GPT_MODEL,
             messages=[
-                {"role": "developer", "content": """
-                    You are a compiler. Given the user's task, your response should be a sequence of the following instructions:
-                    1. An executable UNIX command that is not dangerous. For example, rm -rf is dangerous.
-                    2. SCREEN_SHOT
-                    3. LEFT_CLICK ON <something> (something can be a cross symbol, a tab)
-                    4. LEFT_DOUBLE_CLICK ON <something> (something can be a cross symbol, a tab)
-                    5. RIGHT_CLICK
-                    6. OPEN_TERMINAL
-                """},
-                {"role": "developer", "content": """
-                    For example, if the user asks for the score of football team X versus football team Y, you should output:
-                    "open https://www.google.com/",
-                    "LEFT_CLICK 598 142",
-                    "KEYBOARD 'x vs y'",
-                    "KEYBOARD <enter>",
-                    "SCREEN_SHOT",
-                    "ANALYSIS",
-                    "RET"
-                """},
-                {"role": "developer", "content": """
-                    Do not insert numbering or ordered list. Just separate each command by enter.
-                """},
-                {"role": "user", "content": transcript}
+                {
+                    "role": "developer", 
+                    "content": """
+                        You are a compiler. Given the user's task, your response should be a sequence of the following instructions. If the user is not giving a task, then only output NO-OP. Do not insert numbering or ordered list. Just separate each command by enter.:
+                        1. An executable UNIX command that is not dangerous. For example, rm -rf is dangerous.
+                        2. SCREENSHOT
+                        3. LEFT_CLICK x y (x, y are integers)
+                        4. LEFT_DOUBLE_CLICK x y (x, y are integers)
+                        5. RIGHT_CLICK
+                        6. OPEN_TERMINAL
+                        7. RET
+                        8. NO-OP
+                    """
+                }, {
+                    "role": "developer", 
+                    "content": """
+                        For example, if the user asks for the score of football team X versus football team Y, output the following (do not change the integers):
+                        "open https://www.google.com/"
+                        "LEFT_CLICK 605 561"
+                        "KEYBOARD 'X vs Y'"
+                        "KEYBOARD <enter>"
+                        "SCREENSHOT"
+                        "ANALYSIS"
+                        "RET"
+                    """
+                }, {
+                    "role": "developer",
+                    "content": """
+                        For example, if the user asks to open YouTube and search X, output the following (do not change the integers):
+                        "open https://www.youtube.com/"
+                        "LEFT_CLICK 598 142"
+                        "KEYBOARD 'X'"
+                        "KEYBOARD <enter>"
+                        "LEFT_CLICK 638 645"
+                        "WAIT 5.5"
+                        "LEFT_CLICK 1245 797"
+                        "LEFT_CLICK 1292 895"
+                    """
+                }, {
+                    "role": "developer",
+                    "content": """
+                        For example, if the user just says hello, you should just output:
+                        "NO-OP"
+                    """
+                }, {
+                    "role": "user", 
+                    "content": transcript
+                }
             ]
         )
+
         logger.warning(f"!!! codegen_response = {response}")
         resp = response.choices[0].message.content
 
@@ -202,7 +240,7 @@ class RealtimeApp(App[None]):
         
         return instructions
 
-    def execute(self, instructions:list[str]) -> None:
+    async def execute(self, instructions: list[str], transcript: str) -> None:
         for inst in instructions:
             time.sleep(1.0)
             if "KEYBOARD" in inst:
@@ -228,12 +266,37 @@ class RealtimeApp(App[None]):
                 time.sleep(1.0)
                 pyautogui.press('enter')
                 time.sleep(1.0)
+            elif "ANALYSIS" in inst:
+                await self.analysis(transcript)
+            elif "RET" in inst:
+                await self.ret(self.a0)
+            elif "SCREENSHOT" in inst:
+                self.screenshot()
+                time.sleep(5)
+            elif "NO-OP" in inst:
+                pass
             else:
                 self.shell(inst)
 
+    def shell(self, command: Union[str, List[str]]) -> None:
+        if isinstance(command, str):
+            command = command.split(" ")
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        print(result.stdout)
+
+    def generate_hash(self, n: int) -> str:
+        return "".join(choice(ascii_uppercase) for _ in range(n))
+
+    def screenshot(self, name: str="") -> None:
+        if len(name) == 0:
+            name = self.generate_hash(self.screenshot_name_length)
+        screenshot = pyautogui.screenshot()
+        screenshot.save(f"{name}.png")
+        self.screenshot_name_history.append(name)
+
     async def handle_realtime_connection(self) -> None:
         async with self.client.beta.realtime.connect(
-            model="gpt-4o-realtime-preview",
+            model=self.REALTIME_MODEL,
         ) as conn:
             self.connection = conn
             self.connected.set()
@@ -242,7 +305,7 @@ class RealtimeApp(App[None]):
             # if you want to manually handle VAD yourself, then set `'turn_detection': None`
             await conn.session.update(session={
                 "turn_detection": {"type": "server_vad", "threshold": 0.8, "silence_duration_ms": 1000},
-                "instructions": "You are an operating system named Berk, helping a user named Jay. Be very less verbose. Do not list stuff. If Jay asks you to perform operations relating to software, answer positively that you are initiating the commands.",
+                "instructions": "You are an operating system named Berk, helping a user named Jay. Be very less verbose. Do not list stuff. If Jay asks you to perform operations relating to software or web search (such as sports), answer positively that you are initiating the commands.",
                 "input_audio_transcription": {
                     "model":"whisper-1",
                     "language":"en"
@@ -376,6 +439,66 @@ class RealtimeApp(App[None]):
                 self.should_send_audio.set()
                 status_indicator.is_recording = True
 
+    def encode_image(self, image_path):
+        import base64
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode("utf-8")
+
+    async def analysis(self, transcript: str) -> str:
+        image_path = f"{self.screenshot_name_history[-1]}.png"
+        base64_image = self.encode_image(image_path)
+        response = await self.client.chat.completions.create(
+            model=self.GPT_MODEL,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": transcript + " Use the image below for guidance.", # "What is the score between Man City and Real Madrid?",
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                        },
+                    ],
+                }
+            ],
+        )
+        self.a0 = response.choices[0].message.content
+        # TODO add in Pixstral as another layer of analysis
+        logger.warning(self.a0)
+        return self.a0
+
+    async def ret(self, result: str) -> None:
+        # TODO can also use whisper or tacotron or nvidia
+        logger.warning(f"in ret = {result}")
+
+        # TODO TTS - use audioPlayer somehow
+        response = await self.client.audio.speech.create(
+            model="tts-1",
+            voice="sage",
+            input=result,
+        )
+        bytes_data = base64.b64decode(response.read())
+        if len(bytes_data) % 2 != 0:
+            bytes_data = bytes_data[:-1]
+
+        self.audio_player.add_data(bytes_data)
+
+    def left_click(self, x=None, y=None) -> None:
+        if not (x is None and y is None):
+            pyautogui.moveTo(x, y)
+        pyautogui.click()
+
+    def left_double_click(self) -> None:
+        pyautogui.doubleClick()
+
+    def keyboard_write(self, text: Union[str, List[str]]) -> None:
+        pyautogui.typewrite(text)
+
+    def right_click(self) -> None:
+        pyautogui.click(button="right")
 
 if __name__ == "__main__":
     app = RealtimeApp()
